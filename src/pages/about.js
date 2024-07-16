441cf81ae56b6a2c5ee6cd0205592c3de38feb89
evan-chernicky <echernicky@gmail.com>
2024-07-16 14:29:15 -0400

    added in ravel video in about again

--- src/pages/about.js
+++ src/pages/about.js
@@ -275,7 +275,7 @@ const IndexPage = ({ data }) => (
             webroll={bgVideoColor}
             video={
               <iframe
-                src="https://www.youtube.com/embed/C0DPdy98e4c?si=GcDz4Q0q-af1DK3I&autoplay=1"
+                src="https://www.youtube.com/embed/ppuLLTdbTKk?si=IkUn1o5_1h-AiUw1&autoplay=1"
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 title="Watch The Experience"
                 allowFullScreen
